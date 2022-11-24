df82253f6c26de3903c0a413f8402c2a71f9e57a
Ренат Ренат <renat.churbanov@gmail.com>
2022-11-24 16:21:36 +0300

    Добавьте файлы проекта.

---
+++ LanguageSkillsAPI/Data/Entities/Card.cs
@@ -0,0 +1,15 @@
+﻿namespace LanguageSkillsAPI.Data.Entities
+{
+    public class Card
+    {
+        public int Id;
+        public string? Word;
+        public string? Example { get; set; }
+        public DateTime LastRepeat { get; set; }
+        public DateTime NextRepeat { get; set; }
+        public int RepeatCounter { get; set; }
+        public int Rating { get; set; }
+        public byte StudyStage { get; set; }
+        public bool IsActive;
+    }
+}
---
+++ LanguageSkillsAPI/Data/Entities/CardTranslation.cs
@@ -0,0 +1,14 @@
+﻿using LanguageSkillsAPI.Data.Infrastructure.Enums;
+
+
+namespace LanguageSkillsAPI.Data.Entities
+{
+    public class CardTranslation
+    {
+        public int Id;
+        public int OriginalId;
+        public string? ExampleTranslation { get; set; }
+        public languages Language;
+        public string? Translate;
+    }
+}
